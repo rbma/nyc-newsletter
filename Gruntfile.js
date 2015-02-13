e9e8a094dbb05635a19755fab42db01ea8fea1e7
James Pants <jamespants@gmail.com>
2015-02-13 16:51:27 +0100

    missing png

--- Gruntfile.js
+++ Gruntfile.js
@@ -322,7 +322,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            '/fonts/*'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
@@ -366,8 +367,7 @@ module.exports = function (grunt) {
       ],
       dist: [
         'sass',
-        'copy:styles',
-        'imagemin'
+        'copy:styles'
       ]
     }
   });
