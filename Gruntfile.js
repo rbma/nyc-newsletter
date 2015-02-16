7f8bdcfc65bac164bcb227416a2763729fcf8e70
James Pants <jamespants@gmail.com>
2015-02-16 12:12:45 +0100

    move fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -336,6 +336,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
+            'fonts/*',
             'styles/fonts/{,*/}*.*'
           ]
         }, {
